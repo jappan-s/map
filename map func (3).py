11e2f6053c83b0db68d96b1958f4db81fc7fdce9
jappan-s <63873951+jappan-s@users.noreply.github.com>
2022-11-07 00:31:12 +0530

    Add files via upload

---
+++ map func (3).py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+num =  [4, 5, 2, 9]
+print("Sample list: ", num)
+sq = map(lambda x: x**2, num) 
+print("Square of list numbers:")
+print(list(sq))
+
+
+# In[ ]:
+
+
+
+
